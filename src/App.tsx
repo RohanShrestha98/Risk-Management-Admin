f8d4540427d95371a0557f8f0066f3e2ec6b8880
RohanShrestha98 <nahorshrestha@gmail.com>
2025-04-29 11:45:09 -0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,13 @@ import BaseLayout from "./layouts/BaseLayout";
 import Login from "./pages/auth/login";
 import AuthLayout from "./layouts/AuthLayout";
 import Users from "./pages/users/Users";
-import { useAuthStore } from "./store/useAuthStore";
 import Dashboard from "./pages/dashboard/Dashboard";
-import MyActions from "./pages/myactions/MyActions";
 import RiskTable from "./pages/risktable/RiskTable";
 import User from "./pages/user/User";
-import Task from "./pages/task/Task";
 import Settings from "./pages/settings/Settings";
 import Notification from "./pages/notification/Notification";
 
 function App() {
-  const { user } = useAuthStore();
-
   return (
     <BrowserRouter>
       <Routes>
@@ -26,15 +21,11 @@ function App() {
         <Route element={<BaseLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/user" element={<User />} />
-          <Route path="/my-actions" element={<MyActions />} />
           <Route path="/risk" element={<RiskTable />} />
-          <Route path="/task" element={<Task />} />
           <Route path="/risk-details" element={<Users />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/notification" element={<Notification />} />
         </Route>
-        {/* {user && ( */}
-        {/* )} */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
