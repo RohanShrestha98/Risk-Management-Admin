fabdf79221d35dbe6c2140cb968f21ecfb33d41e
RohanShrestha98 <nahorshrestha@gmail.com>
2025-04-29 15:39:21 -0500

    change

--- src/pages/dashboard/UserTrends.tsx
+++ src/pages/dashboard/UserTrends.tsx
@@ -14,15 +14,13 @@ import {
 export default function UserTrends() {
   const { data: userData } = useUserData();
 
-  const admin = userData?.data?.filter((item) => item?.role?.id == "1");
-  const analyst = userData?.data?.filter((item) => item?.role?.id == "2");
-  const midLevelAnalyst = userData?.data?.filter(
-    (item) => item?.role?.id == "3"
-  );
+  const admin = userData?.data?.filter((item) => item?.role?.id == 1);
+  const analyst = userData?.data?.filter((item) => item?.role?.id == 2);
+  const midLevelAnalyst = userData?.data?.filter((item) => item?.role?.id == 3);
   const executiveLevelAnalyst = userData?.data?.filter(
-    (item) => item?.role?.id == "4"
+    (item) => item?.role?.id == 4
   );
-  const iso = userData?.data?.filter((item) => item?.role?.id == "5");
+  const iso = userData?.data?.filter((item) => item?.role?.id == 5);
 
   const data = [
     {
